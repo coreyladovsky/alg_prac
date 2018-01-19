ac8301fe9bc305f7d6aa6c3648405f95425cfe6c
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-19 17:15:10 -0500

    BSTNODE class and BST insert working

--- BST/lib/binary_search_tree.rb
+++ BST/lib/binary_search_tree.rb
@@ -1,11 +1,35 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
 
+require("bst_node")
+
 class BinarySearchTree
+  attr_reader :root
   def initialize
+    @root = nil
   end
 
-  def insert(value)
+  def insert(value, node = @root)
+    if node.nil?
+      @root = BSTNode.new(value)
+      return
+    else
+      if node.value > value
+        if node.left
+          insert(value, node.left)
+        else
+          node.left = BSTNode.new(value)
+        end
+      else
+        if node.value < value
+          if node.right
+            insert(value, node.right)
+          else
+            node.right = BSTNode.new(value)
+          end
+        end
+      end
+    end
   end
 
   def find(value, tree_node = @root)
@@ -19,7 +43,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +54,9 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+
+  def child_insert(node, value)
+
+  end
 
 end
--- BST/lib/bst_node.rb
+++ BST/lib/bst_node.rb
@@ -1,4 +1,7 @@
 class BSTNode
+  attr_accessor :left, :right, :value
+
   def initialize(value)
+    @value = value
   end
 end
