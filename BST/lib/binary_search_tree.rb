b0718cff7cda8dacd62ee17841841319ae70800e
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-22 17:45:17 -0500

    working on traversal"

--- BST/lib/binary_search_tree.rb
+++ BST/lib/binary_search_tree.rb
@@ -107,6 +107,24 @@ class BinarySearchTree
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    if tree_node.left.nil? && tree_node.right.nil?
+      return arr.push(tree_node.value)
+    end
+
+    if tree_node.left
+      left = in_order_traversal(tree_node.left, arr)
+      arr.concat(left)
+    # else
+    #   left = []
+    end
+    if tree_node.right
+      right = in_order_traversal(tree_node.right, arr)
+      arr.concat(right)
+    # else
+      # right = []
+    end
+
+    return arr
   end
 
 
