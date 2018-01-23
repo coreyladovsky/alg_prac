115aad9ebddf284291ffdc1bb4f361158746d502
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-23 12:06:10 -0500

    kth largest

--- BST/lib/bst_practical.rb
+++ BST/lib/bst_practical.rb
@@ -2,4 +2,8 @@
 require 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  bst = BinarySearchTree.new
+  arr = bst.in_order_traversal(tree_node)
+  val = arr[k * -1]
+  bst.find(val, tree_node)
 end
